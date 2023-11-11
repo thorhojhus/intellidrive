b032f8a1b854dec8a634e576a8bcf3c9bffeae2d
Thor Hojhus <thorhojhus@gmail.com>
2023-11-11 16:48:23 +0100

    update to readme

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 # ROS2 Autonomous Robot Project
 
-I'm trying to model a RC-car with ackermann steering for simulation with Gazebo and ROS2 Humble.
+Trying to model a RC-car with Ackermann steering for simulation with Gazebo and ROS2 Humble.
 
-Goal is to control a RC-car with Lidar and Depth Sensor for autonomous driving and navigation using SLAM. 
+Goal is to control a RC-car with Lidar and Depth Sensor for autonomous driving and navigation using SLAM-algorithms provided by ROS2. 
+
+This is a work-in-progress. 
+
+To build and source:
+```bash
+colcon build --symlink-install 
+source install/setup.bash
+```
+
+To launch simple sim in gazebo with diff_drive controller and lidar 
+```bash
+ros2 launch my_bot launch_sim.launch.py
+```
+
+To control messages are published on /cmd_vel topic and can be controlled with
+```bash
+ros2 run teleop_twist_keyboard teleop_twist_keyboard
+```
+
+To launch rviz2 and gazebo with Ackermann Car Steering controller
+```bash
+ros2 launch my_bot bringup.launch.py
+```
+
+Inspect topics with 
+```bash
+rqt-graph
+```
--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -14,13 +14,25 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     package_name='my_bot'
+    pkg = get_package_share_directory('my_bot')
+    launch_dir = os.path.join(pkg, 'launch')
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
-
+    
+    world_config = DeclareLaunchArgument(
+          'world',
+          default_value=[os.path.join(pkg, 'worlds', 'simple.world'), ''],
+          description='SDF world file')
+    
+    rviz_config_path = os.path.join(pkg, 'config', 'view_bot.rviz')
+    start_rviz2_cmd = ExecuteProcess(
+        cmd=['rviz2', '--display-config', rviz_config_path],
+        cwd=[launch_dir],
+        output='screen')
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -37,7 +49,9 @@ def generate_launch_description():
 
     # Launch them all!
     return LaunchDescription([
+        world_config,
         rsp,
         gazebo,
         spawn_entity,
+        start_rviz2_cmd
     ])
